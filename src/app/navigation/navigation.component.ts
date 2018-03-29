e1bf50e7fbf3f6d50946c5cd1cb8a26410487b4a
Tyler Moon <moon.tyler@gmail.com>
2018-03-28 22:06:40 -0400

    Cleaned up navigation and added ng-templates

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, NavigationEnd } from '@angular/router';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -7,7 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  links = [
+    {
+      title: 'Home',
+      link: '/home',
+      active: false,
+    }, {
+      title: 'Services',
+      link: '/services',
+      active: false,
+    }, {
+      title: 'Contact',
+      link: '/contact',
+      active: false,
+    }, {
+      title: 'About',
+      link: '/about',
+      active: false,
+    }, {
+      title: 'Blog',
+      link: '/blog',
+      active: false,
+    }
+  ]
+
+  constructor(private router: Router) {
+    router.events.filter(event => event instanceof NavigationEnd)
+    .subscribe((event) => {
+      for( let i = 0; i < this.links.length; i++ ) {
+        if(this.links[i].link === (event as NavigationEnd).url){
+          this.links[i].active = true;
+        } else {
+          this.links[i].active = false;
+        }
+      }
+    })
+  }
 
   ngOnInit() {
   }
