5eae6c5fc6d83bfc3c00f52f715a29164baf3b4c
Tyler Moon <moon.tyler@gmail.com>
2018-03-28 21:37:54 -0400

    Cleaned up home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // Data for the cards
+  cards = [
+    {
+        'title': 'Web Development',
+        'text': 'Some quick example text to build on the card\
+                title and make up the bulk of the card\'s content.',
+        'bulletPoints': [
+          'A point',
+          'Another point',
+          'Yay points',
+          'another one',
+        ],
+        'route': '/services',
+    } , {
+        'title': 'Wordpress Setup',
+        'text': 'Some quick example text to build on the card\
+                title and make up the bulk of the card\'s content.',
+        'bulletPoints': [
+          'A point',
+          'Another point',
+          'Yay points',
+          'another one',
+        ],
+        'route': '/services',
+    } , {
+        'title': 'Feature Development',
+        'text': 'Some quick example text to build on the card\
+                title and make up the bulk of the card\'s content.',
+        'bulletPoints': [
+          'A point',
+          'Another point',
+          'Yay points',
+          'another one',
+        ],
+        'route': '/services',
+    } , {
+        'title': 'Technical Consulting',
+        'text': 'Some quick example text to build on the card\
+                title and make up the bulk of the card\'s content.',
+        'bulletPoints': [
+          'A point',
+          'Another point',
+          'Yay points',
+          'another one',
+        ],
+        'route': '/services',
+    },
+  ]
 
-  ngOnInit() {
+  // Which bootstrap col to use for the cards
+  bootstrapCol:string;
+
+
+  constructor() {
+    // Which bootstrap col to use for the cards. Determined by 12 / # of cards
+    // Ex. col-md-6 if there are 2 cards
+    this.bootstrapCol = 'col-md-' + (12 / this.cards.length).toString();
   }
+
+  ngOnInit() {}
 
 }
