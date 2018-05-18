9dcbb939eceabf2cc988592f3c9e9d58b4b0e2bf
Lawton Mizell <alcamech@gmail.com>
2018-05-18 11:59:10 -0400

    added content for home page cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,23 +20,30 @@ export class HomeComponent implements OnInit {
     [
       {
           'title': 'Web Development',
-          'text': 'Some quick example text to build on the card\
-                  title and make up the bulk of the card\'s content.',
+          'text': 'Your website should be the forefront of your\
+                   business or project in the digital space. We promise to\
+                   deliver websites that are professionally made,\
+                   technically advanced and esthetically appealing.',
           'icon': 'file-text',
       } , {
           'title': 'Wordpress Setup',
-          'text': 'Some quick example text to build on the card\
-                  title and make up the bulk of the card\'s content.',
+          'text': 'Want a website using the most popular blogging\
+                   platform in the market? We got your covered.\
+                   We deliver customizable Wordpress sites or blogs.',
           'icon': 'wordpress',
       } , {
           'title': 'Feature Development',
-          'text': 'Some quick example text to build on the card\
-                  title and make up the bulk of the card\'s content.',
+          'text': 'Need your current website revised or improved?\
+                   We can update your site, develop new features or\
+                   improve upon an existing code base.',
           'icon': 'bolt',
       } , {
           'title': 'Technical Consulting',
-          'text': 'Some quick example text to build on the card\
-                  title and make up the bulk of the card\'s content.',
+          'text': 'Want consultation ? Come to us. We do consultation \
+                  on your website and how it can be improved or built.\
+                  In addition to using the best technology available.\
+                  We also do consultation on your Social Media prescence\
+                  and how to improve your online prescence. ',
           'icon': 'lightbulb-o',
       },
     ]
